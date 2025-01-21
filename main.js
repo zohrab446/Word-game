d6bad9ccf37ba14d5141fde47824a0884d84dc7e
Zohrab <116635367+zohrab446@users.noreply.github.com>
2025-01-21 14:48:03 +0300

    js change
    few js change

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const word_el = document.getElementById('word');
 const popup = document.getElementById('popup-container');
 const message_el =document.getElementById('success-message');
-const correctLetters = ['j', 'a', 'v'];
-const worngLetters = []
+const correctLetters = [];
+const wrongLetters = []
+let selectedWord = getRandomWord();
+const wrongLetter_el =document.getElementById('wrong-letters');
+const items = document.querySelectorAll('.item');
+const message_el2 = document.getElementById('message');
+const playBtn = document.getElementById('play-again');
 
 function getRandomWord(){
     const words = ["java","javascirpt",'python'];
@@ -13,8 +18,7 @@ function getRandomWord(){
 console.log(getRandomWord());
 
 function displayWord(){
-    const selectedWord = getRandomWord();
-
+      
     word_el.innerHTML =`
        ${selectedWord.split('').map(letter =>`
        <div class="letter">
@@ -31,4 +35,65 @@ function displayWord(){
 
 
 }
+
+function updateWrongLetters(){
+    wrongLetter_el.innerHTML = `
+    ${wrongLetters.length>0? '<h3>Hatali Harfler</h3>':''}
+    ${wrongLetters.map(letter => `<span> ${letter} <span>`)}
+    `;
+    items.forEach((item,index) => {
+        const errorCount = wrongLetters.length;
+        if(index<errorCount){
+            item.style.display ='block';
+        } else{
+            item.style.display = 'none';
+        }
+    })
+
+    if(wrongLetters.length === items.length){
+        popup.style.display = 'flex';
+        message_el.innerText = 'Maalesef kaybettiniz';
+    }
+}
+
+function displayMessage(){
+    message_el2.classList.add('show');
+    setTimeout(function(){
+          message_el2.classList.remove('show')        
+    },2000)
+}
+
+ playBtn.addEventListener('clcik',function(){
+     correctLetters.splice(0);
+     wrongLetters.splice(0);
+
+     selectedWord = getRandomWord();
+      displayWord();
+      updateWrongLetters();
+
+      popup.style.display = 'none';
+});
+
+window.addEventListener('keydown', function(e){
+    if(e.keyCode>= 65 && e.keyCode <= 90){
+    const letter= e.key;
+    if(selectedWord.includes(letter)){
+        if(!correctLetters.includes(letter)){
+            correctLetters.push(letter);
+            displayWord();
+        }else{
+            displayMessage();
+             message_el2.classList.add('show');
+        }
+    }else{
+        if(!wrongLetters.includes(letter)){
+            wrongLetters.push(letter);
+            updateWrongLetters();
+        }else{
+            displayMessage();
+        }
+    }
+}
+});
+
 displayWord();
